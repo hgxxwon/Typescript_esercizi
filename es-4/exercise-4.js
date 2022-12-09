132e645d2a082f06d16ade07369c2a576eeb0781
mirko <autieromir@gmail.com>
2022-12-09 01:16:53 +0100

    type aliases

---
+++ es-4/exercise-4.js
@@ -0,0 +1,9 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+exports.__esModule = true;
+var country1 = "Nigeria";
+var country2 = "Italy";
+var country3 = "China";
+console.log(country1, country2, country3);
