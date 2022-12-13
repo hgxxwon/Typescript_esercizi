a497bf1dbe8deb2b8c43869132fb93b7444b057e
mirko <autieromir@gmail.com>
2022-12-13 11:08:46 +0100

    es-15

--- es-15/exercise-1-complete-the-class-start.ts
+++ es-15/exercise-1-complete-the-class-start.ts
@@ -7,9 +7,9 @@ class Currency {
     // Hint: Look at the type of values being passed when `new Currency()`
     // is called below.
 
-    name;
-    code;
-    symbol;
+    private name = "";
+    private code: string;
+    private symbol:string = "";
 
     // Add types to the constructor parameters.
 
@@ -18,7 +18,8 @@ class Currency {
 
         // Store the `code` and `symbol` parameter values in
         // their corresponding class fields.
-
+        this.code = code;
+        this.symbol = symbol;
 
     }
 
@@ -28,13 +29,13 @@ class Currency {
     // Add a return type for this method
     // Hint: Look at the inferred return type.
 
-    describe() {
+    public describe(): any {
         // Replace the `null` values below with the appropriate field values.
         // Hint: Access field values with: this.fieldName
 
-        let description = `The ${null} currency `;
-        description += `has the code ${null} `;
-        description += `and uses the symbol ${null}.`;
+        let description = `The ${this.name} currency `;
+        description += `has the code ${this.code} `;
+        description += `and uses the symbol ${this.symbol}.`;
 
         console.log(description);
     }
@@ -48,7 +49,7 @@ console.log(currencyNaira);
 
 // Call the `describe()` method on the `currencyNaira` object.
 
-
+const describe = currencyNaira.describe();
 
 // ----
 
@@ -58,7 +59,7 @@ console.log(currencyUsDollar);
 
 // Call the `describe()` method on the `currencyUsDollar` object.
 
-
+const describe2 = currencyUsDollar.describe();
 
 // ----
 
